43a1a1e00db857998621486dddf0222f14644465
Viktor Yankov <roterabe@yahoo.com>
2019-05-15 22:36:47 +0300

    Some swag.

---
+++ Uvod/hw09/binary_insertion.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <sys/types.h>
+
+using namespace std;
+
+/*int rand_int(){
+    return rand() % 20 + 1;
+}*/
+
+int binarySearch(int a[], int item, int low, int high) 
+{ 
+    if (high <= low) 
+        return (item > a[low])?  (low + 1): low; 
+  
+    int mid = (low + high)/2; 
+  
+    if(item == a[mid]) 
+        return mid+1; 
+  
+    if(item > a[mid]) 
+        return binarySearch(a, item, mid+1, high); 
+    return binarySearch(a, item, low, mid-1); 
+} 
+
+void insertionSort(int arr[], int n)  
+{  
+    int Buf, j, check;  
+    for (int i = 1; i < n; i++) 
+    {  
+        Buf = arr[i]; 
+        j = i - 1;  
+
+        check = binarySearch(arr, Buf, 0, j); 
+
+        while (j >= check) 
+        {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;  
+        }  
+        arr[j + 1] = Buf;  
+    }  
+}  
+
+int rand_int(){
+    return rand() % 20 + 1;
+}
+
+int main(){
+    time_t  t0, t1;
+
+
+    t0 = time(NULL);
+    clock_t begin = clock();
+
+    int k = 10000;
+    int rev_count = 10000;
+    int arr[k];
+
+    for (int i = 0; i < k; i++){
+      arr[i] = rev_count;
+      rev_count--;
+    }   
+  
+    insertionSort(arr, k);  
+    clock_t end = clock();
+
+    t1 = time(NULL);
+
+    double answer = (double) (end - begin)/CLOCKS_PER_SEC;
+
+    //std::cout << "Elapsed wall clock time: " << ((long) (t1 - t0)) << endl;
+    std::cout << "Elapsed wall clock time :" << answer << endl;
+    return 0;  
+}  
--- Uvod/testcase.cpp
+++ Uvod/testcase.cpp
@@ -1,89 +1,70 @@
-#include <algorithm>
 #include <iostream>
-#include <ctime>
-#include <cstdio>
-
+#include <vector>
+#include <algorithm>
+#include <time.h>
+#include <sys/types.h>
+#include <stdio.h>
 using namespace std;
 
-int main()
-{
-	int j = 0;
-	int k = 0;
-	bool InvCount;
-	int ind = 0;
-	int ind2 = 0;
-	int mid = 0;
-	int buff = 0;
-	int check = 2;
-	int tmp = 0;
-	int x = 0;
-	int end = 0;
-	const int N = 10000;
-	int arr[N];
+void insertionSort(int arr[], int n)  
+{  
+    int Buf, j;  
+    for (int i = 1; i < n; i++) 
+    {  
+        Buf = arr[i]; 
+        j = i - 1;  
 
-	for (int h = 0; h < mid; h++) {
-		arr[h] = h;
-		x++;
-	}
-	for (int k = N; k > mid; k--) {
-		arr[x] = k;
-		x++;
-	}
+        while (j >= 0 && arr[j] > Buf) 
+        {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;
+			//cout << arr[j+1] << " ";  
+        }  
+        arr[j + 1] = Buf;  
+		//cout << endl << arr[i-1] << " ";
+    }  
+} 
 
-	end = N - ind;
 
-	clock_t startTime = clock();
-
-	for (int i = 0; i < end; i++) {
-		InvCount = false;
-
-		while (j < N - 1) {
-			if (arr[j] > arr[j + 1]) {
-				k = arr[j + 1];
-				InvCount = true;
-				ind = j;
-				end = N - ind;
-				break;
-			}
-			j++;
-		}
-		if (InvCount == false) {
-			break;
-		}
-
-		if (ind % 2 == 0) {
-			mid = ind / 2;
-		}
-		else mid = (ind / 2) + 1;
-
-		for (int h = 0; h < ind; h++) {
-			if (k > arr[mid] && k < arr[mid + 1]) {
-				ind2 = mid + 1;
-				break;
-			}
-			if (k < arr[mid]) {
-				check += 2;
-				mid = ind / check;
-			}
-			else if (k > arr[mid]) {
-				check += 2;
-				tmp = ind / check;
-				mid = mid + tmp;
-			}
-		}
-		check = 2;
-		buff = arr[ind + 1];
-
-		for (int g = ind + 1; g > ind2; g--) {
-			arr[g] = arr[g - 1];
-		}
-
-		arr[ind2] = buff;
-	}
-
-	clock_t endTime = clock();
-
-	cout << (double)(endTime - startTime) / CLOCKS_PER_SEC << endl;
-
-	return 0;
+int rand_int(){
+    return rand() % 20 + 1;
 }
+
+void print(int arr[], int k){
+	for (int i = 0; i < k; i++) {
+		cout << arr[i] << " ";
+	}
+}
+
+
+    int main(){
+        
+        int k = 10;
+        int arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        //for (int i = 0; i < k; i++){
+        //arr[i]=rand_int();
+    //}  
+  
+
+    time_t  t0, t1;
+    clock_t c0, c1;
+
+   // t0 = time(NULL);
+    c0 = clock();
+
+    insertionSort(arr, k);  
+
+    c1 = clock();
+   // t1 = time(NULL);
+
+    //reverse(a.begin(), a.end());
+    //srand ( time(NULL) );
+
+
+    //cout << "Elapsed wall clock time: " << ((long) (t1 - t0)) << endl;
+    cout << "Elapsed wall clock time :" << (double)(c1 - c0)/CLOCKS_PER_SEC << endl << endl;
+    print(arr, k);
+  
+    return 0;  
+}  

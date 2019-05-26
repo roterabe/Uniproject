a69b67c218258a139a8dfbef7f84e6294c3abbff
Viktor Yankov <roterabe@yahoo.com>
2019-05-26 19:00:51 +0300

    Finally works, *cries in happiness*.

--- Test3/compute.cpp
+++ Test3/compute.cpp
@@ -9,10 +9,50 @@ using namespace std;
 class Computation
 {
 public:
-	virtual int compute() = 0;
+	//Computation(char comp);
+	virtual int compute(vector<int> data) = 0;
 
 private:
+	vector<int> data = vector<int>();
 };
+
+class Sum : public Computation
+{
+public:
+	Sum();
+	//Sum(vector<int> data);
+	virtual int compute(vector<int> data);
+
+private:
+	int result;
+	vector<int> data = vector<int>();
+};
+
+class Sumeven : public Computation
+{
+public:
+	Sumeven();
+	//Sumdiv(vector<int> data);
+	virtual int compute(vector<int> data);
+
+private:
+	int result;
+	vector<int> data = vector<int>();
+};
+
+class Min : public Computation
+{
+public:
+	Min();
+	virtual int compute(vector<int> data);
+
+private:
+	int result;
+	vector<int> data = vector<int>();
+};
+
+Computation *
+newcompute(string compu);
 
 // ---------------------------------
 
@@ -50,6 +90,8 @@ int main(int argc, char const *argv[])
 }
 
 // Define functions and member functions here
+
+// Saves data in vector.
 vector<int> readDataFromFile(string filepath)
 {
 	int num = 0;
@@ -61,6 +103,76 @@ vector<int> readDataFromFile(string filepath)
 	}
 	return nums;
 }
+
+// Saves computation type in vector and calls a new object as needed.
 vector<Computation *> readComputationsFromFile(string filepath)
 {
+	string comp;
+	vector<Computation *> operations = vector<Computation *>();
+	ifstream oper(filepath, ios_base::in);
+	while (oper >> comp)
+	{
+		// Error when read vitual function called.
+		auto computation = newcompute(comp);
+		operations.push_back(computation);
+	}
+	return operations;
+}
+
+// Computes what object to call.
+Computation *newcompute(string compu)
+{
+	string sum1 = "sum";
+	string sum_even = "sum-even";
+	string e_min = "min";
+	if (sum1 == compu)
+		return new Sum();
+	else if (sum_even == compu)
+		return new Sumeven();
+	else if (e_min == compu)
+		return new Min();
+}
+
+Sum::Sum()
+{
+}
+
+Sumeven::Sumeven()
+{
+}
+
+int Sum::compute(vector<int> data)
+{
+	int sum = 0;
+	for (auto e : data)
+	{
+		sum += e;
+	}
+	return sum;
+}
+
+int Sumeven::compute(vector<int> data)
+{
+	int sum = 0;
+	for (int i = 0; i < data.size(); i++)
+	{
+		if (data[i] % 2 == 0)
+			sum += data[i];
+	}
+	return sum;
+}
+
+Min::Min()
+{
+}
+
+int Min::compute(vector<int> data)
+{
+	int minimal = data[0];
+	for (int i = 0; i < data.size(); i++)
+	{
+		if (minimal > data[i])
+			minimal = data[i];
+	}
+	return minimal;
 }

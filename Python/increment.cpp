8fceca2f4281c604528ce5766c98142808fc0635
Viktor Yankov <roterabe@yahoo.com>
2019-08-10 19:19:49 +0300

    Some infinite tests

---
+++ Python/increment.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int square(int n)
+{
+    int k = 0;
+    while(true)
+    {
+        if (k == n*n)
+        {
+            return k;
+        }
+        k++;
+    }
+}
+
+int main()
+{
+ int n = 100000;
+
+ cout << square(n) << endl;
+
+}

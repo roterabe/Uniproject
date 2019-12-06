b1de2b845fad5d22709a8ad8dd678ce06f84a5b4
Viktor Yankov <roterabe@yahoo.com>
2019-12-06 11:28:59 +0200

    Test - kirov 12.2019.

--- Kirov/blank.cpp
+++ Kirov/blank.cpp
@@ -1,24 +1,85 @@
 #include <iostream>
-#include <vector>
-#include <list>
 using namespace std;
 
-template <typename T>
-T maxA(T *arr, int s = 2)
+/* A binary tree node has data, pointer to left child 
+and a pointer to right child */
+struct Node
 {
-  T maxVal = arr[0];
-  for (int i = 1; i < s; i++)
-    if (arr[i] > maxVal)
-      maxVal = arr[i];
-  return maxVal;
+  char data;
+  struct Node *left, *right;
+  Node(char data)
+  {
+    this->data = data;
+    left = right = NULL;
+  }
+};
+
+/* Given a binary tree, print its nodes according to the 
+"bottom-up" postorder traversal. */
+void printPostorder(struct Node *node)
+{
+  if (node == NULL)
+    return;
+
+  // first recur on left subtree
+  printPostorder(node->left);
+
+  // then recur on right subtree
+  printPostorder(node->right);
+
+  // now deal with the node
+  cout << node->data << " ";
 }
 
+/* Given a binary tree, print its nodes in inorder*/
+void printInorder(struct Node *node)
+{
+  if (node == NULL)
+    return;
+
+  /* first recur on left child */
+  printInorder(node->left);
+
+  /* then print the data of node */
+  cout << node->data << " ";
+
+  /* now recur on right child */
+  printInorder(node->right);
+}
+
+/* Given a binary tree, print its nodes in preorder*/
+void printPreorder(struct Node *node)
+{
+  if (node == NULL)
+    return;
+
+  /* first print data of node */
+  cout << node->data << " ";
+
+  /* then recur on left sutree */
+  printPreorder(node->left);
+
+  /* now recur on right subtree */
+  printPreorder(node->right);
+}
+
+/* Driver program to test above functions*/
 int main()
 {
-  long *a = new long[2];
-  cout << maxA(a);
-  vector<int> k[4, 7, 2];
-  cout << k.erase(2);
+  struct Node *root = new Node('a');
+  root->left = new Node('c');
+  root->right = new Node('e');
+  root->left->left = new Node('d');
+  root->right->right = new Node('b');
+
+  cout << "\nPreorder traversal of binary tree is \n";
+  printPreorder(root);
+
+  cout << "\nInorder traversal of binary tree is \n";
+  printInorder(root);
+
+  cout << "\nPostorder traversal of binary tree is \n";
+  printPostorder(root);
 
   return 0;
 }

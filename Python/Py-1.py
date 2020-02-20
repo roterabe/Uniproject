c5a2adc78d40156eeec42ac411cf42b9f96cc453
Viktor Yankov <roterabe@yahoo.com>
2020-02-20 15:33:29 +0200

    Check logic.

--- Python/Py-1.py
+++ Python/Py-1.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a != 0:
+    print("yep")
+    if b**2-4*a*c > 0:
+        print("x1 & x2")
+
+else:
+    if b != 0:
+        print("bx+c=0")
+        print("x1 = -c/b")
+    else:
+        print("nope - 0x+c-0")
+

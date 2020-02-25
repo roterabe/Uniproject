50aac1436e7c767428a145b750ab0897bad3f633
Viktor Yankov <roterabe@yahoo.com>
2020-02-25 12:35:27 +0200

    Some additional exercises.

--- Java/Week2/src/com/company/Main.java
+++ Java/Week2/src/com/company/Main.java
@@ -2,7 +2,41 @@ package com.company;
 
 public class Main {
 
+    public static void DisplayDivisors(int number) {
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static int SumofDivisors(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static void isPerfect(int sum, int number) {
+        if (sum == number) {
+            System.out.println("The number " + sum + " is perfect.");
+        } else {
+            System.out.println("The number " + number + " is not perfect.");
+        }
+    }
+
+    public static void checkPerfect(int res){
+
+    }
+
     public static void main(String[] args) {
+        int number = 28;
+        DisplayDivisors(number);
         System.out.println();
+        System.out.println(SumofDivisors(number));
+        isPerfect(SumofDivisors(number), number);
     }
 }

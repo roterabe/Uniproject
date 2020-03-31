77a90c0db60c74a39a2c1405868c2445bca3ee74
Viktor Yankov <roterabe@yahoo.com>
2020-03-31 20:24:20 +0300

    Setup python environment and wxPython.

--- Python/Random/wxPython.py
+++ Python/Random/wxPython.py
@@ -2,25 +2,26 @@
 import wx
 import os
 
+
 class MainWindow(wx.Frame):
     def __init__(self, parent, title):
-        self.dirname=''
+        self.dirname = ''
 
         # A "-1" in the size parameter instructs wxWidgets to use the default size.
         # In this case, we select 200px width and the default height.
-        wx.Frame.__init__(self, parent, title=title, size=(200,-1))
+        wx.Frame.__init__(self, parent, title=title, size=(200, -1))
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.CreateStatusBar() # A Statusbar in the bottom of the window
+        self.CreateStatusBar()  # A Statusbar in the bottom of the window
 
         # Setting up the menu.
-        filemenu= wx.Menu()
-        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open"," Open a file to edit")
-        menuAbout= filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-        menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+        filemenu = wx.Menu()
+        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open", " Open a file to edit")
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
+        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
 
         # Creating the menubar.
         menuBar = wx.MenuBar()
-        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        menuBar.Append(filemenu, "&File")    # Adding the "filemenu" to the MenuBar
         self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
 
         # Events.
@@ -39,22 +40,22 @@ class MainWindow(wx.Frame):
         self.sizer.Add(self.control, 1, wx.EXPAND)
         self.sizer.Add(self.sizer2, 0, wx.EXPAND)
 
-        #Layout sizers
+        # Layout sizers
         self.SetSizer(self.sizer)
         self.SetAutoLayout(1)
         self.sizer.Fit(self)
         self.Show()
 
-    def OnAbout(self,e):
+    def OnAbout(self, e):
         # Create a message dialog box
         dlg = wx.MessageDialog(self, " A sample editor \n in wxPython", "About Sample Editor", wx.OK)
-        dlg.ShowModal() # Shows it
-        dlg.Destroy() # finally destroy it when finished.
+        dlg.ShowModal()  # Shows it
+        dlg.Destroy()   # finally destroy it when finished.
 
-    def OnExit(self,e):
+    def OnExit(self, e):
         self.Close(True)  # Close the frame.
 
-    def OnOpen(self,e):
+    def OnOpen(self, e):
         """ Open a file"""
         dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", ".", wx.FD_OPEN)
         if dlg.ShowModal() == wx.ID_OK:
@@ -64,6 +65,7 @@ class MainWindow(wx.Frame):
             self.control.SetValue(f.read())
             f.close()
         dlg.Destroy()
+
 
 app = wx.App(False)
 frame = MainWindow(None, "Sample editor")

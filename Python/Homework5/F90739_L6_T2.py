0edb294d7de250976f27e44253b9dc53f568fa2a
Viktor Yankov <roterabe@yahoo.com>
2020-04-02 00:42:46 +0300

    Some issues later.

--- Python/Homework5/F90739_L6_T2.py
+++ Python/Homework5/F90739_L6_T2.py
@@ -27,39 +27,39 @@ class Hangman(wx.Frame):
         self.cnt = 6
         # self.jpg = wx.Image("6.bmp", wx.BITMAP_TYPE_ANY)
         # self.image = wx.StaticBitmap(self, -1, self.jpg, (10 + self.jpg.GetWidth(), 5), (self.jpg.GetWidth(), self.jpg.GetHeight()))
-        # self.imagеCtrl = wx.ImageFromBitmap(self.jpg)   
+        # self.imagеCtrl = wx.ImageFromBitmap(self.jpg)
         self.pick.Bind(wx.EVT_BUTTON, self.randClick)
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         # self.display = wx.TextCtrl(self, -1, '',  style=wx.TE_RIGHT)
         # sizer.Add(self.display, 0, wx.EXPAND | wx.TOP | wx.BOTTOM, 4)
         gs = wx.GridSizer(4, 7, 1, 1)
-        gs.AddMany([(wx.Button(self, 1, 'q'), 0, wx.EXPAND),
-                    (wx.Button(self, 2, 'w'), 0, wx.EXPAND),
-                    (wx.Button(self, 3, 'e'), 0, wx.EXPAND),
-                    (wx.Button(self, 4, 'r'), 0, wx.EXPAND),
-                    (wx.Button(self, 5, 't'), 0, wx.EXPAND),
-                    (wx.Button(self, 6, 'y'), 0, wx.EXPAND),
-                    (wx.Button(self, 7, 'u'), 0, wx.EXPAND),
-                    (wx.Button(self, 8, 'i'), 0, wx.EXPAND),
-                    (wx.Button(self, 9, 'o'), 0, wx.EXPAND),
-                    (wx.Button(self, 10, 'p'), 0, wx.EXPAND),
-                    (wx.Button(self, 11, 'a'), 0, wx.EXPAND),
-                    (wx.Button(self, 12, 's'), 0, wx.EXPAND),
-                    (wx.Button(self, 13, 'd'), 0, wx.EXPAND),
-                    (wx.Button(self, 14, 'f'), 0, wx.EXPAND),
-                    (wx.Button(self, 15, 'g'), 0, wx.EXPAND),
-                    (wx.Button(self, 16, 'h'), 0, wx.EXPAND),
-                    (wx.Button(self, 17, 'j'), 0, wx.EXPAND),
-                    (wx.Button(self, 18, 'k'), 0, wx.EXPAND),
-                    (wx.Button(self, 19, 'l'), 0, wx.EXPAND),
-                    (wx.Button(self, 20, 'z'), 0, wx.EXPAND),
-                    (wx.Button(self, 21, 'x'), 0, wx.EXPAND),
-                    (wx.Button(self, 22, 'c'), 0, wx.EXPAND),
-                    (wx.Button(self, 23, 'v'), 0, wx.EXPAND),
-                    (wx.Button(self, 24, 'b'), 0, wx.EXPAND),
-                    (wx.Button(self, 25, 'n'), 0, wx.EXPAND),
-                    (wx.Button(self, 26, 'm'), 0, wx.EXPAND)])
+        gs.AddMany([(wx.Button(self, 1, 'a'), 0, wx.EXPAND),
+                    (wx.Button(self, 2, 'b'), 0, wx.EXPAND),
+                    (wx.Button(self, 3, 'c'), 0, wx.EXPAND),
+                    (wx.Button(self, 4, 'd'), 0, wx.EXPAND),
+                    (wx.Button(self, 5, 'e'), 0, wx.EXPAND),
+                    (wx.Button(self, 6, 'f'), 0, wx.EXPAND),
+                    (wx.Button(self, 7, 'g'), 0, wx.EXPAND),
+                    (wx.Button(self, 8, 'h'), 0, wx.EXPAND),
+                    (wx.Button(self, 9, 'i'), 0, wx.EXPAND),
+                    (wx.Button(self, 10, 'j'), 0, wx.EXPAND),
+                    (wx.Button(self, 11, 'k'), 0, wx.EXPAND),
+                    (wx.Button(self, 12, 'l'), 0, wx.EXPAND),
+                    (wx.Button(self, 13, 'm'), 0, wx.EXPAND),
+                    (wx.Button(self, 14, 'n'), 0, wx.EXPAND),
+                    (wx.Button(self, 15, 'o'), 0, wx.EXPAND),
+                    (wx.Button(self, 16, 'p'), 0, wx.EXPAND),
+                    (wx.Button(self, 17, 'q'), 0, wx.EXPAND),
+                    (wx.Button(self, 18, 'r'), 0, wx.EXPAND),
+                    (wx.Button(self, 19, 's'), 0, wx.EXPAND),
+                    (wx.Button(self, 20, 't'), 0, wx.EXPAND),
+                    (wx.Button(self, 21, 'u'), 0, wx.EXPAND),
+                    (wx.Button(self, 22, 'v'), 0, wx.EXPAND),
+                    (wx.Button(self, 23, 'w'), 0, wx.EXPAND),
+                    (wx.Button(self, 24, 'x'), 0, wx.EXPAND),
+                    (wx.Button(self, 25, 'y'), 0, wx.EXPAND),
+                    (wx.Button(self, 26, 'z'), 0, wx.EXPAND)])
 
         self.Bind(wx.EVT_BUTTON, self.onClick, id=1)
         self.Bind(wx.EVT_BUTTON, self.onClick, id=2)
@@ -97,7 +97,7 @@ class Hangman(wx.Frame):
         self.Centre()
 
     def randClick(self, event):
-        dictionary = ["banana", "apple", "strawberry", "mango", "kiwi"]
+        dictionary = ["meaning", "life", "towel", "dolphin", "mice"]
         self.rem = random.choice(dictionary)
         chars = list(self.rem)
         for i in range(1, len(chars)-1):
@@ -108,7 +108,7 @@ class Hangman(wx.Frame):
 
     def onClick(self, event):
         if self.cnt > 0:
-            letters = "qwertyuiopasdfghjklzxcvbnm"
+            letters = "abcdefghijklmnopqrstuvwxyz"
             letters = list(letters)
             btn = event.GetId()
             rem = list(self.rem)
@@ -124,6 +124,8 @@ class Hangman(wx.Frame):
             self.hiddenword = ''.join(hid)
             # print(self.hiddenword)
             self.word.SetLabel(str(hid))
+            if '_' not in hid:
+                self.word.SetLabel("***" + str(hid) + "***")
         elif self.cnt <= 0:
             self.word.SetLabel("Game Over")
 

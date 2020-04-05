403f9d59c06844800bee4d00416b6298374615fe
Viktor Yankov <roterabe@yahoo.com>
2020-04-05 19:26:20 +0300

    Finished overall code.

--- Algorithms/Homework2/tree.cpp
+++ Algorithms/Homework2/tree.cpp
@@ -1,39 +1,95 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-void heapify(int a[], int n, int i)
+// Heapify structure of array, swap and call recursively.
+void maxHeapify(int a[], int n, int p)
 {
-    int largest = i;
-    int l = 3 * i + 1;
-    int m = 3 * i + 2;
-    int r = 3 * i + 3;
+    int largest = p;
+    int l = 3 * p + 1;
+    int m = 3 * p + 2;
+    int r = 3 * p + 3;
 
-    if (l > n && a[l] > a[largest])
+    if (l < n && a[l] > a[largest])
     {
         largest = l;
     }
 
-    if (m > n && a[m] > a[largest])
+    if (m < n && a[m] > a[largest])
     {
         largest = m;
     }
 
-    if (r > n && a[r] > a[largest])
+    if (r < n && a[r] > a[largest])
     {
         largest = r;
     }
 
-    if (largest != i)
+    if (largest != p)
     {
-        swap(largest, i);
+        swap(a[p], a[largest]);
 
-        heapify(a, n, largest);
+        maxHeapify(a, n, largest);
     }
+}
+
+// Separate code for building heap structure. For visual representation.
+void buildMaxHeap(int arr[], int n)
+{
+    for (int i = n / 3 - 1; i >= 0; --i)
+        maxHeapify(arr, n, i);
+}
+
+//Base logic of sort.
+void heapSort(int arr[], int n)
+{
+    buildMaxHeap(arr, n);
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+
+        maxHeapify(arr, i, 0);
+    }
+}
+
+//Print array.
+void print(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
+
+// Shuffle array values.
+void shuffle(int a[], int n)
+{
+    random_shuffle(&a[0], &a[n]);
 }
 
 int main()
 {
+    int n = 0;
+    cout << "Enter array size: ";
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = i + 1;
+    }
+
+    cout << "This is the sorted array:"
+         << "\n";
+    print(a, n);
+    cout << "This is the same array, shuffled:"
+         << "\n";
+    shuffle(a, n);
+    print(a, n);
+    cout << "This is the array sorted with a ternary heap sort:"
+         << "\n";
+    heapSort(a, n);
+    print(a, n);
 
     return 0;
 }

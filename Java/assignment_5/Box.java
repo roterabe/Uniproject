4e7ebb7b51b1ca455901683d6d94b95e96cdc573
Viktor Yankov <roterabe@yahoo.com>
2020-04-08 17:50:48 +0300

    Construct box and calc volume.

--- Java/assignment_5/Box.java
+++ Java/assignment_5/Box.java
@@ -1,4 +1,16 @@
 
 public class Box {
+    int w,h,d;
 
+    Box(int w,int h,int d){
+        this.w = w;
+        this.h = h;
+        this.d = d;
+    }
+
+    int volume(){
+        int vol = 0;
+        vol = w*h*d;
+        return vol;
+    }
 }

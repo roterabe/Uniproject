55cde738d01504432d39c6e0af4e6138a4cb81c6
Viktor Yankov <roterabe@yahoo.com>
2020-04-08 18:17:07 +0300

    Construct base for Element.

--- Java/assignment_5/Element.java
+++ Java/assignment_5/Element.java
@@ -1,4 +1,25 @@
 
-public class Element {
+public class Element extends Material {
+    int weight;
+    String name;
+    boolean fragile;
+    Material material;
 
+    Element(Material material, String name, boolean fragile, int weight) {
+        super(name, fragile);
+        this.weight = weight;
+
+    }
+
+    void isFragile() {
+        if (fragile == true) {
+            System.out.println("The " + name + " is fragile.");
+        } else
+            System.out.println("The " + name + " is not fragile.");
+    }
+
+    public String toString()
+    {
+        return "Element is made from: " + material.toString() + "and weighs: " + weight + ".kg.";
+    }
 }

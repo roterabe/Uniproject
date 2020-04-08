f8b6475a3846a6c482fe54495681b2524fd618e3
Viktor Yankov <roterabe@yahoo.com>
2020-04-08 18:16:12 +0300

    Construct base for material.

--- Java/assignment_5/Material.java
+++ Java/assignment_5/Material.java
@@ -1,4 +1,13 @@
 
 public class Material {
+    boolean fragile;
+    String name;
 
+    Material(String name, boolean fragile) {
+        
+    }
+
+   public String toString(){
+        return "Element name: " + name + ". Is fragile?: " + fragile + ".";
+    }
 }

b054481766668adb840961b463c35de0a6f336cd
Viktor Yankov <roterabe@yahoo.com>
2020-04-08 18:16:56 +0300

    Construct base for company.

--- Java/assignment_5/CourierCompany.java
+++ Java/assignment_5/CourierCompany.java
@@ -1,4 +1,25 @@
 
 public class CourierCompany {
+    String name;
+    int money = 0;
 
+    CourierCompany(String name) {
+        this.name = name;
+    }
+
+    void addItem() {
+
+    }
+
+    void totalEarnings() {
+
+    }
+
+    void totalVolume() {
+
+    }
+
+    public String toString(){
+        return "The company is: " + name + " and it has made a total of: " + money + ".lv.";
+    }
 }

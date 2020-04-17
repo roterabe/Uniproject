e1f868fe4a7503e1d9deb89b7badb5a9be03368e
Viktor Yankov <roterabe@yahoo.com>
2020-04-17 12:03:38 +0300

    Base for homework.

---
+++ Java/assignment_6/Contract.java
@@ -0,0 +1,9 @@
+
+public enum Contract {
+    PERMANENT(10), PARTTIME(5), TRAINEE(2);
+    double inc;
+
+    Contract(double inc) {
+        this.inc = inc;
+    }
+}
---
+++ Java/assignment_6/Employee.java
@@ -0,0 +1,7 @@
+
+public class Employee {
+    String name;
+    int id = 0;
+    double hoursMonth = 0;
+
+}

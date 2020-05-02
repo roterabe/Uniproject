69537d420370c46f70e0ad7e22064ad1d1e5e2db
Viktor Yankov <roterabe@yahoo.com>
2020-05-03 00:20:38 +0300

    Small changes.

--- Python/Homework9/F90739_L10_T1.py
+++ Python/Homework9/F90739_L10_T1.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-import sys
-import os
+from sys import argv
 
 rfile = ""
 wfile = ""
+
 try:
-    if len(sys.argv) > 2:
-        rfile = sys.argv[1]
-        wfile = sys.argv[2]
+    if len(argv) > 2:
+        rfile = argv[1]
+        wfile = argv[2]
     else:
         raise
 except:
-    print("\n" "You need to specify a file to read and file to write to.")
+    print("\nWARNING! \nYou need to specify a file to read and file to write to.")
     print("Example usage: \"python F90739_L10_T1.py -filetoread.txt -filetowrite.txt\"." "\n")
 
 with open(rfile, 'r') as file:
--- Python/Homework9/F90739_L10_T2.py
+++ Python/Homework9/F90739_L10_T2.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 
-import sys
-import os
-import requests
+from sys import argv
 
 stem = ""
 word = ""
+d = {}
 
 try:
-    if len(sys.argv) > 2:
-        rfile = sys.argv[1]
-        wfile = sys.argv[2]
+    if len(argv) > 2:
+        stem = argv[1]
+        word = argv[2]
     else:
         raise
 except:
-    print("\n" "You need to specify a file to read and file to write to.")
-    print("Example usage: \"python F90739_L10_T1.py -filetoread.txt -word_to_search\"." "\n")
+    print("\nWARNING! \nYou need to specify a file to read and file to write to.")
+    print("Example usage: \"python F90739_L10_T1.py -filetoread.txt -word_to_search.\n")
 
 with open(stem, "r") as file:
-    lines = file.readlines()
+    lines = file.read().splitlines()
 
+for line in lines:
+    x = line.split(":")
+    d[x[0]] = x[1]
+
+try:
+    print(d[word])
+except:
+    print("\n The word you want to find is not available in the database. Try another word instead. \n")

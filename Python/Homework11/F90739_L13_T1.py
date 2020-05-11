b4a721481ac8358a6f94073f839244f9bb48cd79
Viktor Yankov <roterabe@yahoo.com>
2020-05-12 00:38:11 +0300

    Made base window for app with title list.

--- Python/Homework11/F90739_L13_T1.py
+++ Python/Homework11/F90739_L13_T1.py
@@ -14,12 +14,47 @@ text = {}
 for item in root.findall('{http://purl.org/rss/1.0/}item'):
     items.append(item)
 for item in items:
-#    print(list(item))
-    temp = {item.find('{http://purl.org/rss/1.0/}title').text : item.find('{http://purl.org/rss/1.0/}description').text}
+    temp = {item.find('{http://purl.org/rss/1.0/}title').text: item.find(
+        '{http://purl.org/rss/1.0/}description').text}
     text.update(temp)
 
-print(text.values())
-#for line in lines:
-#    print(line)
+
+class News(wx.Frame):
+    def __init__(self, parent, id):
+
+        super().__init__(
+            None, title="News Feed",
+            size=(800, 600))
+        self.SetBackgroundColour("White")
+        self.panel = wx.Panel(self, size=(500, 500))
+        self.panel.SetBackgroundColour("Gray")
+
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(self.panel, flag=wx.ALIGN_LEFT |
+                  wx.EXPAND | wx.BOTTOM, border=10)
+
+        self.temp = []
+        self.index = 0
+
+        self.titles = wx.ListCtrl(self.panel, pos=(
+            20, 100), size=(500, 300), style=wx.LC_REPORT)
+        self.titles.InsertColumn(0, 'Title', width=480)
+
+        for title in text.keys():
+            self.temp.append(title)
+
+        for title in self.temp:
+            self.titles.InsertStringItem(self.index, title)
+            self.index += 1
+
+        self.titles.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onLeftClick)
+
+    def onLeftClick(self, event):
+        pos = event.GetText()
+        print(str(pos))
 
 
+app = wx.App()
+frame = News(None, -1)
+frame.Show()
+app.MainLoop()

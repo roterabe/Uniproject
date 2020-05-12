7af070cbedcbb07c81d903e5ad1e636a724567a2
Viktor Yankov <roterabe@yahoo.com>
2020-05-12 16:54:19 +0300

    Finished hw with listbox and htmlwindow.

--- Python/Homework11/F90739_L13_T1.py
+++ Python/Homework11/F90739_L13_T1.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import wx
+import wx.html
 import urllib.request
-import requests
 import xml.etree.ElementTree as xml
 
 url = 'http://rss.slashdot.org/Slashdot/slashdot'
@@ -22,36 +22,59 @@ for item in items:
 class News(wx.Frame):
     def __init__(self, parent, id):
 
+        no_resize = wx.DEFAULT_FRAME_STYLE & ~ (wx.RESIZE_BORDER |
+                                                wx.MAXIMIZE_BOX)
+
         super().__init__(
-            None, title="News Feed",
-            size=(800, 600))
+            None, title="News Feed", style=no_resize, size=(976, 500))
         self.SetBackgroundColour("White")
-        self.panel = wx.Panel(self, size=(500, 500))
-        self.panel.SetBackgroundColour("Gray")
+        self.panel = wx.Panel(self)
+        self.panel.SetBackgroundColour("White")
+        self.info = wx.Panel(self)
+        self.info.SetBackgroundColour("White")
 
         sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(self.panel, flag=wx.ALIGN_LEFT |
-                  wx.EXPAND | wx.BOTTOM, border=10)
 
         self.temp = []
+        self.temp1 = []
         self.index = 0
+        self.head = wx.StaticText(self.panel, label="News Feed", pos=(8, 10))
+        font = wx.Font(18, wx.DECORATIVE, wx.NORMAL, wx.BOLD)
+        self.head.SetFont(font)
+        self.ver = wx.StaticText(self.info, label="ver 0.1a", pos=(420, 430))
+        font1 = wx.Font(10, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        self.ver.SetFont(font1)
 
-        self.titles = wx.ListCtrl(self.panel, pos=(
-            20, 100), size=(500, 300), style=wx.LC_REPORT)
-        self.titles.InsertColumn(0, 'Title', width=480)
+        self.html = wx.html.HtmlWindow(self.info, pos=(5, 50), size=(470, 300),
+                                       style=wx.html.HW_DEFAULT_STYLE)
+
+        sizer.Add(self.panel, flag=wx.EXPAND | wx.ALL)
+        sizer.Add(self.info, flag=wx.EXPAND | wx.ALL)
 
         for title in text.keys():
             self.temp.append(title)
+        for description in text.values():
+            description = description.split('<p>', 1)[0]
+            self.temp1.append(description)
 
-        for title in self.temp:
-            self.titles.InsertStringItem(self.index, title)
-            self.index += 1
+        self.titles = wx.ListBox(self.panel, pos=(
+            8, 50), size=(470, 300), choices = self.temp)
 
-        self.titles.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onLeftClick)
+        self.titles.Bind(wx.EVT_LISTBOX, self.onLeftClick)
+
+        self.SetSizer(sizer)
+        self.Centre()
 
     def onLeftClick(self, event):
-        pos = event.GetText()
-        print(str(pos))
+        titl = event.GetString()
+        desc = ""
+        for i in range(len(self.temp)):
+            if titl == self.temp[i]:
+                desc = self.temp1[i]
+        html = "<html>\n <head></head>\n <body> <p>" + desc + "</p></body>\n </html>"
+        with open('article.html', 'w') as file:
+            file.writelines(html)
+        self.html.SetPage(html)
 
 
 app = wx.App()

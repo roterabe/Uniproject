a65fd2279421cc3020ba0a436e376fae434841e7
Viktor Yankov <roterabe@yahoo.com>
2020-11-21 19:01:12 +0200

    Fixed gallery layout for now.

--- Client-Server/ex/gal.js
+++ Client-Server/ex/gal.js
@@ -1,2 +1,25 @@
-Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.6.1/themes/classic/galleria.classic.min.js');
-Galleria.run('.galleria');
+// Initialize array for the images
+var data = ["roman.jpg", "avatar.jpg", "profile.jpg"];
+
+
+Galleria.run('.galleria', {
+    // Div with "galleria" class <-- we'll load images here
+    dataSource: data,
+    responsive: true,
+    height: 0.65,
+    thumbnails: 'lazy', // lazy load thumbnails, in chunkcs
+    lightbox: true, // use lightbox for images on mouseclick
+    lightboxTransitionSpeed: 50,
+    lightboxFadeSpeed: 50,
+    preload: 4
+});
+
+Galleria.ready(function() {
+    // keyboard navigation 
+    this.attachKeyboard({
+        right: this.next,
+        left: this.prev,
+    });
+    // lazy loading thumbnails, in chunks, for performance
+    this.lazyLoadChunks(10)
+});

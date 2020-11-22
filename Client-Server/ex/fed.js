3fa2d954b41b2956d1aa90d96f9eb8532ba36624
Viktor Yankov <roterabe@yahoo.com>
2020-11-23 01:10:26 +0200

    minor changes

--- Client-Server/ex/fed.js
+++ Client-Server/ex/fed.js
@@ -1,13 +1,17 @@
 function savef() {
 
     var data = 'data=' + document.getElementById('feedback').value;
-    var feedbback = document.getElementById('feedback').value;
+    var feed = document.getElementById('feedback').value;
     var xmlhttp = new XMLHttpRequest();
 
     xmlhttp.open('POST', 'savef.php', true);
 
     xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xmlhttp.send(data);
-
+    feed = document.createTextNode(feed);
+    let p = document.createElement('p');
+    p.className = 'lab';
+    p.appendChild(feed);
+    $('#wraps').append(p);
     $('#feedback').val('');
 }

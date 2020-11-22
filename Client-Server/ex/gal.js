034ffd398d0addfa72ee8eb50620d047b1be278d
Viktor Yankov <roterabe@yahoo.com>
2020-11-22 13:53:11 +0200

    Final touches on gallery + downloads initial fixes.

--- Client-Server/ex/gal.js
+++ Client-Server/ex/gal.js
@@ -1,25 +1,4 @@
-// Initialize array for the images
-var data = ["roman.jpg", "avatar.jpg", "profile.jpg"];
-
-
-Galleria.run('.galleria', {
-    // Div with "galleria" class <-- we'll load images here
-    dataSource: data,
-    responsive: true,
-    height: 0.65,
-    thumbnails: 'lazy', // lazy load thumbnails, in chunkcs
-    lightbox: true, // use lightbox for images on mouseclick
-    lightboxTransitionSpeed: 50,
-    lightboxFadeSpeed: 50,
-    preload: 4
-});
-
-Galleria.ready(function() {
-    // keyboard navigation 
-    this.attachKeyboard({
-        right: this.next,
-        left: this.prev,
-    });
-    // lazy loading thumbnails, in chunks, for performance
-    this.lazyLoadChunks(10)
-});
+$(document).ready(function() {
+    Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.6.1/themes/folio/galleria.folio.min.js');
+    Galleria.run('.galleria');
+}());
